1ee46a7dc4dbfeaede4c46c4c49becd1a10c11a2
Dex <dexter.i.horthy@gmail.com>
2017-12-11 14:32:05 -0800

    private image

--- release/k8s/scripts/install.sh
+++ release/k8s/scripts/install.sh
@@ -2,18 +2,22 @@
 
 NS='{{ ConfigOption "namespace" }}'
 
+docker load docker/api.tar
+docker tag quay.io/superbigtool/api:2.1.0 {{ ConfigOption "private_registry_api" }}:2.1.0
+docker push {{ ConfigOption "private_registry_api" }}:2.1.0
+
 kubectl create secret generic postgres \
   --from-literal=POSTGRES_USER={{ ConfigOption "postgres_user" }} \
   --from-literal=POSTGRES_PASSWORD={{ ConfigOption "postgres_password" }} \
   --from-literal=POSTGRES_HOST={{ ConfigOption "postgres_host" }} \
   --from-literal=POSTGRES_PORT={{ ConfigOption "postgres_port" }}
 
-
 kubectl create secret generic jwt \
   --from-literal=HMAC_SECRET={{ ConfigOption "hmac_secret" }}
 
 kubectl create namespace "$NS"
 kubectl apply --namespace "$NS" -f ./k8s/
+
 
 echo "
 
